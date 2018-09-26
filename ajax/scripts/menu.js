c276a9df9fcebdd200cbf624baaddd504b34e052
PryFork <43276364+PryFork@users.noreply.github.com>
2018-09-26 14:12:58 -0400

    fixed responsive menu scripts

---
+++ ajax/scripts/menu.js
@@ -0,0 +1,29 @@
+function topNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+    console.log("Loaded Responsive Top Navigation Menu")
+}
+
+function navBar() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+    console.log("Loaded Navigation Menu")
+}
+
+function lowNav() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+    console.log("Loaded Bottom Navigation Menu")
+}
